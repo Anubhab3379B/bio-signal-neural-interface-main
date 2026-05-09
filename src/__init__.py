741a93f96a6853f3ae84c59c3eab035fada69784
Anubhab3379B <anubhab3379b@users.noreply.github.com>
2026-05-10 04:50:05 +0545

    Automated Drip: src/__init__.py

---
+++ src/__init__.py
@@ -0,0 +1,10 @@
+"""
+Main package init file for bio_signal_interface
+"""
+
+# Version information
+__version__ = '0.1.0'
+__author__ = 'Bio-Signal Interface Research Team'
+__description__ = 'Bio-Signal & Human Interface System for Neural Prosthetics Research'
+
+# This file marks the src directory as a Python package
